418e13386570bf01a7abb74160d8b08e5e620ff7
Madhav-msit-044 <72123358+Madhav-msit-044@users.noreply.github.com>
2021-07-19 15:20:15 +0530

    circlesintersect

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -5,5 +5,6 @@
 # the triangleArea function you just wrote above.
 
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
+     area=0.5*( (x1*(y2-y3)) + (x2*(y3-y1)) + (x3*(y1-y2)) )
+     return area
 	# your code goes here
-	pass
