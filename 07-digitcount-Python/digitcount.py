036c446c1b69e637d7b9e2f43f2bace49b85b4b0
Madhav-msit-044 <72123358+Madhav-msit-044@users.noreply.github.com>
2021-07-27 15:04:34 +0530

    alternating sum

--- 07-digitcount-Python/digitcount.py
+++ 07-digitcount-Python/digitcount.py
@@ -6,5 +6,13 @@
 # repeatedly remove the ones digit until you cannot.
 
 def digitcount(n):
+    n=abs(n)
+    c=0
+    if(n==0):
+        return 1
+    else:
+        while (n>0):
+            n//=10
+            c+=1
+        return c
 	# your code goes here
-	pass
