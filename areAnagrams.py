b48aa976965e4d848313c78a18bd82476167de3b
Madhav-msit-044 <72123358+Madhav-msit-044@users.noreply.github.com>
2021-08-04 13:01:55 +0530

    recursion_secondlargest

--- areAnagrams.py
+++ areAnagrams.py
@@ -10,8 +10,16 @@
 # which could be quite handy here.
 # Hint: The time complexity can be achieved in Linear.
 
+import collections
 def areAnagrams(s1, s2):
+    a=s1.lower()
+    b=s2.lower()
+    x=collections.Counter(a)
+    y=collections.Counter(b)
+    return x==y
+
+print(areAnagrams(input(), input()))
+
     # Your code goes here...
-    pass
 
 # write your test cases here...
